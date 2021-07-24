2570b3f3f8ce3558302525de65bc077a2de7563d
densebear <74503398+DenseBear@users.noreply.github.com>
2021-07-23 20:56:15 -0500

    changed within android studio 4 files added 3 modified 2 deleted

--- app/src/main/java/gg/mic/aysi_milaria/MainActivity.java
+++ app/src/main/java/gg/mic/aysi_milaria/MainActivity.java
@@ -1,6 +1,5 @@
 package gg.mic.aysi_milaria;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -9,42 +8,43 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Color;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Pair;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.soundcloud.android.crop.Crop;
-
-import org.tensorflow.lite.Interpreter;
-
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import gg.mic.aysi_milaria.ml.NewModel;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Interpreter tflite;
-    private ArrayList<String> labelList = new ArrayList<String>();
-    public Button execute_button, capture_button;
+    PopupWindow popUp;
+    public Button execute_button;
+    public ImageButton capture_button;
     private Uri imageUri;
     private ImageView imageView;
+    private Bitmap img;
 
     public static final int REQUEST_IMAGE = 100;
+    public static final int REQUEST_CAMERA_ROLL = 3645;
     public static final int REQUEST_PERMISSION = 300;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,9 +55,15 @@ public class MainActivity extends AppCompatActivity {
         execute_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println(tflite);
                 openCameraIntent();
 
+            }
+        });
+
+        capture_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openStorageIntent();
             }
         });
 
@@ -76,21 +82,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     private void init(){
-
-        labelList.add("Uninfected");
-        labelList.add("Parasitized");
-
-        try {
-            tflite = new Interpreter(Helper.loadModelFile(this.getAssets()), new Interpreter.Options());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         imageView = (ImageView) findViewById(R.id.imageView);
         execute_button = (Button) findViewById(R.id.predict_button);
         execute_button.setText("Predict");
+
+        capture_button = (ImageButton) findViewById(R.id.capture_image);
 
         requestPermissions();
 
@@ -119,60 +117,130 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    private void openStorageIntent(){
+        Intent intent = new Intent((Intent.ACTION_GET_CONTENT));
+        intent.setType("image/*");
+
+        startActivityForResult(intent, REQUEST_CAMERA_ROLL);
+
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        System.out.println("Picture Taken");
 
-        if(requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_IMAGE && resultCode == RESULT_OK && imageUri != null) {
             try {
                 Uri source_uri = imageUri;
                 Uri dest_uri = Uri.fromFile(new File(getCacheDir(), "cropped"));
                 Crop.of(source_uri, dest_uri).asSquare().start(this);
+                img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-        else if(requestCode == Crop.REQUEST_CROP && resultCode == RESULT_OK){
+        if(requestCode == REQUEST_CAMERA_ROLL && resultCode == RESULT_OK){
+
+
+            imageView.setImageURI(data.getData());
+            Uri source_uri = data.getData();
+            Uri dest_uri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+            Crop.of(source_uri, dest_uri).asSquare().start(this);
+
+            try {
+                img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+        if(requestCode == Crop.REQUEST_CROP && resultCode == RESULT_OK){
+            imageView.setImageURI(Crop.getOutput(data));
             classifyImage(Crop.getOutput(data));
         }
     }
 
     private void classifyImage(Uri uri) {
-        int imageSizeX = Helper.DIM_IMG_SIZE_X;
-        int imageSizeY = Helper.DIM_IMG_SIZE_Y;
-        int imagePixelSize = Helper.DIM_PIXEL_SIZE;
-
-        // initialize array that holds image data
-        int[] imgArray = new int[imageSizeX * imageSizeY];
-        System.out.println(imgArray.length);
-        // initialize byte array.
-        ByteBuffer imgData =  ByteBuffer.allocateDirect(4 * imageSizeX * imageSizeY * imagePixelSize);
-        imgData.order(ByteOrder.nativeOrder());
-
-        // initialize probabilities array.
-        float[][] labelProbArray = new float[1][labelList.size()];
+        TensorBuffer outputFeature0 = null;
 
         try {
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-            imageView.setImageBitmap(bitmap);
-        } catch (IOException e) {
+            img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+            img = Bitmap.createScaledBitmap(img, 224, 224, true);
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        // get current bitmap from imageView
-        Bitmap bitmap_orig = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-        // resize the bitmap to the required input size to the CNN
-        Bitmap bitmap = Helper.getResizedBitmap(bitmap_orig, imageSizeX, imageSizeY);
-        // convert bitmap to byte array
-        imgData = Helper.convertBitmapToByteBuffer(bitmap, imgData, imgArray);
-        // pass byte data to the graph
+        try {
+            NewModel model = NewModel.newInstance(getApplicationContext());
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
 
-        System.out.println(imgData);
-        tflite.run(imgData, labelProbArray);
+            TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
+            tensorImage.load(img);
+            ByteBuffer byteBuffer = tensorImage.getBuffer();
+
+            inputFeature0.loadBuffer(byteBuffer);
+            NewModel.Outputs outputs = model.process(inputFeature0);
+            outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            model.close();
+
+            System.out.println(outputFeature0.getFloatArray()[0] + " | " + outputFeature0.getFloatArray()[1]);
+
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        onButtonShowPopupWindowClick(findViewById(android.R.id.content).getRootView(), outputFeature0.getFloatArray()[0], outputFeature0.getFloatArray()[1]);
 
     }
+    public void onButtonShowPopupWindowClick(View view, float uninfected_val, float infected_val) {
+
+
+        LayoutInflater inflater = (LayoutInflater)
+        getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.result, null);
+
+        int width = view.getWidth();
+        int height = view.getHeight();
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        Button back_button = (Button) popupView.findViewById(R.id.back_button);
+
+        TextView uninfected = (TextView) popupView.findViewById(R.id.textView3);
+        TextView infected = (TextView) popupView.findViewById(R.id.textView2);
+
+        TextView label = (TextView) popupView.findViewById(R.id.res);
+        label.setTextColor((uninfected_val > infected_val) ? Color.parseColor("#5AC934") : Color.parseColor("#C54C32"));
+
+        Snackbar snackbar = Snackbar.make(view, "Malaria Infection Results", Snackbar.LENGTH_LONG);
+        snackbar.show();
+
+        uninfected.setText(String.valueOf(uninfected_val * 100) + "%");
+        infected.setText(String.valueOf(infected_val * 100) + "%");
+
+        back_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+
+            }
+        });
+    }
+
 
 
 }
